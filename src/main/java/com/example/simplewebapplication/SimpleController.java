49713d4b2bcc5bbb35bb818ec3ecb38488b67645
KrasyukovPA <Dit2023!>
2024-03-13 19:21:41 +0300

    initial commit

---
+++ src/main/java/com/example/simplewebapplication/SimpleController.java
@@ -0,0 +1,15 @@
+/**
+ * @author administrator on 13.03.2024.
+ */
+package com.example.simplewebapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+    @RequestMapping("/")
+    public String index() {
+	return "Hello Pablo!";
+    }
+}
